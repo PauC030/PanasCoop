0d47b175658a32122c6432f39b763a6536ee081f
pauc030 <paulaandrea30.x@gmail.com>
2025-07-08 10:40:16 -0500

    nuevo login y register

--- client/src/pages/EmailVerificationPage.jsx
+++ client/src/pages/EmailVerificationPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+/*import { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useAuth } from '../context/authContext';
 
@@ -30,7 +30,7 @@ const EmailVerificationPage = () => {
           
           // Redirigir al dashboard o página principal después de 3 segundos
           setTimeout(() => {
-            navigate('/dashboard'); // Cambia esta ruta según tu aplicación
+            navigate('/tasks'); // Cambia a la ruta que desees
           }, 3000);
         } else {
           setVerificationStatus('error');
@@ -124,4 +124,4 @@ const EmailVerificationPage = () => {
   );
 };
 
-export default EmailVerificationPage;
+export default EmailVerificationPage;*/
